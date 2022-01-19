541e284730b57c308d910e5c026fbadb47842b5c
Rozovod <rozovod.store@mail.ru>
2022-01-19 14:54:12 +0300

    2. Отсортировать департаменты [#34352 #252363]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,16 +8,6 @@ public class DepDescComp implements Comparator<String> {
         String[] o1c = o1.split("/");
         String[] o2c = o2.split("/");
         int rsl = o2c[0].compareTo(o1c[0]);
-        if (rsl == 0) {
-            for (int i = 0; i < Math.min(o1c.length, o2c.length); i++) {
-                rsl = o1c[i].compareTo(o2c[i]);
-                if (rsl != 0) {
-                    break;
-                } else {
-                    rsl = Integer.compare(o1c.length, o2c.length);
-                }
-            }
-        }
-        return rsl;
+        return rsl != 0 ? rsl : o1.compareTo(o2);
     }
 }
