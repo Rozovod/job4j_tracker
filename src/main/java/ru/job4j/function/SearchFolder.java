6d2e33faf453058797fb7288dba8e540fad4f0e2
Rozovod <rozovod.store@mail.ru>
2022-01-22 03:17:18 +0300

    1.3. Встроенные функциональные интерфейсы. [#249209]

---
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -0,0 +1,20 @@
+package ru.job4j.function;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchFolder {
+
+    Predicate<Folder> pred = f -> f.getSize() > 100 || f.getName().contains("bug");
+
+    public static List<Folder> filter(List<Folder> list, Predicate<Folder> pred) {
+        List<Folder> rsl = new ArrayList<>();
+        for (Folder f : list) {
+            if (pred.test(f)) {
+                rsl.add(f);
+            }
+        }
+        return rsl;
+    }
+}
