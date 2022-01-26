68d19925c5731f3f62cee059ec8fac942db05655
Rozovod <rozovod.store@mail.ru>
2022-01-26 17:34:52 +0300

    8. Перенос методов в Stream API [#279131]

---
+++ src/main/java/ru/job4j/lambda/Card.java
@@ -0,0 +1,35 @@
+package ru.job4j.lambda;
+
+import java.util.stream.Stream;
+
+enum Suit {
+    Diamonds, Hearts, Spades, Clubs
+}
+
+enum Value {
+    V_6, V_7, V_8
+}
+
+public class Card {
+    private Suit suit;
+    private Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{"
+                + "suit=" + suit
+                + ", value=" + value + '}';
+    }
+
+    public static void main(String[] args) {
+        Stream.of(Suit.values())
+                .flatMap(s -> Stream.of(Value.values())
+                        .map(x -> new Card(s, x)))
+                .forEach(System.out::println);
+    }
+}
