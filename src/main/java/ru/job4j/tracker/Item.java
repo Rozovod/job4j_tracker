8ed7a1f01a684c069a7e9af7529dff2f1f37c832
Rozovod <rozovod.store@mail.ru>
2023-03-20 15:17:28 +0300

    1. Lombok. Настройка IDEA [#504888 #402469]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -8,11 +8,12 @@ import java.time.format.DateTimeFormatter;
 @Data
 @NoArgsConstructor
 @RequiredArgsConstructor
-@AllArgsConstructor
 public class Item {
+    @NonNull
     private int id;
     @NonNull
     private String name;
+    @NonNull
     private LocalDateTime created = LocalDateTime.now();
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 }
